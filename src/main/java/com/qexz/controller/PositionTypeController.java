77cf090221329e14a8554f5f4d73569cc130afbd
yangfukobe <57205691@qq.com>
2021-06-21 01:05:09 +0800

    职位类型json

--- src/main/java/com/qexz/controller/PositionTypeController.java
+++ src/main/java/com/qexz/controller/PositionTypeController.java
@@ -2,15 +2,19 @@ package com.qexz.controller;
 
 import com.qexz.common.QexzConst;
 import com.qexz.dto.AjaxResult;
+import com.qexz.model.Account;
 import com.qexz.model.PositionType;
 import com.qexz.service.PositionTypeService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/positionType")
@@ -50,10 +54,12 @@ public class PositionTypeController {
     /**
      * 分页获取所有职位列表
      */
-    @RequestMapping(value = "/api/getPositions", method = RequestMethod.POST)
+    @RequestMapping(value = "/api/getPositions", method = RequestMethod.GET)
     @ResponseBody
     public AjaxResult getPositionTypes(HttpServletRequest request, HttpServletResponse response) {
         AjaxResult ajaxResult = new AjaxResult();
+        List<PositionType> data = positionTypeService.getPositionTypes();
+        ajaxResult.setData(data);
         return ajaxResult;
     }
 }
